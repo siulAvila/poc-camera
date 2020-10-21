c93a8666c4d1161b51dd7dc7326399ae450206e8
Luis Fernando <luisfernandoavila92@gmail.com>
2020-10-21 15:15:54 -0300

    fix: correcao posicao dos buttons

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,11 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'camera'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('camera');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
